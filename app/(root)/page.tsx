26783785fa56b10787087d59954fa63cb0a0d3b8
Aryan <aryaninmail@gmail.com>
2024-02-03 20:45:17 +0530

    fetchPosts server action implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+import { fetchPosts } from "@/lib/actions/thread.actions";
+
+export default async function Home() {
+  const result = await fetchPosts(1, 30);
+  console.log(result);
+
   return (
     <div>
       <h1 className="head-text text-left">Home</h1>
