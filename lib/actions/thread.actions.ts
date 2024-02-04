4ab462b677a509e011f81929d9c714deb3ad96f9
Aryan <aryaninmail@gmail.com>
2024-02-04 19:58:01 +0530

    thread cards structure created

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,60 +1,108 @@
-'use server';
+"use server";
 
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
-interface Params{
-    text:string;
-    author: string;
-    communityId: string | null;
-    path: string;
+interface Params {
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
 }
-export async function createThread({text, author, communityId, path}: Params){
-    try {
-        connectToDB();
-
-    const createdThread = await Thread.create({
-        text, 
-        author, 
-        community: null
-    });
-
-    // update user model 
-    await User.findByIdAndUpdate(author, {
-        $push: {threads: createdThread._id}
-    })
-
-    revalidatePath(path);
-    } catch (error: any) {
-        throw new Error(`Error creating thread ${error.message}`)
-    }
-}
-
-export async function fetchPosts(pageNumber=1, pageSize=20){
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) {
+  try {
     connectToDB();
 
-    // calculate the number of posts to skip  
-    const skipAmount = (pageNumber-1) * pageSize;
+    const createdThread = await Thread.create({
+      text,
+      author,
+      community: null,
+    });
 
-    // Fetch posts that have no parents(top level threads...)
-    const postsQuery = Thread.find({parentId: {$in: [null, undefined]}})
-    .sort({createdAt: 'desc'})
+    // update user model
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id },
+    });
+
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error creating thread ${error.message}`);
+  }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  // calculate the number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Fetch posts that have no parents(top level threads...)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
     .skip(skipAmount)
     .limit(pageSize)
-    .populate({path:'author', model: User})
-    .populate({path: 'children', populate: {
-        path:'author',
-        model:'User',
-        select: '_id name parentId image'
-    }})
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: "User",
+        select: "_id name parentId image",
+      },
+    });
 
-    const totalPostsCount = await Thread.countDocuments({parentId: {$in: [null, undefined]}})
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
 
-    const posts = await postsQuery.exec();
+  const posts = await postsQuery.exec();
 
-    const isNext = totalPostsCount > skipAmount + posts.length;
+  const isNext = totalPostsCount > skipAmount + posts.length;
 
-    return {posts, isNext};
+  return { posts, isNext };
+}
+
+export async function fetchThreadById(id: string) {
+  connectToDB();
+
+  try {
+    // TODO: populate community
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id id name parentId image",
+          },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Error fetching thread ${error.message}`);
+  }
 }
